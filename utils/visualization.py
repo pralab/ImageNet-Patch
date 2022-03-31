c2166b401fc1dcd556b72d4809dde666515e01ab
DanieleAngioni <angioni.daniele97.00@gmail.com>
2022-03-31 11:04:39 +0200

    plot patches added

--- utils/visualization.py
+++ utils/visualization.py
@@ -26,3 +26,33 @@ def imshow(img, transforms=None):
     plt.figure()
     plt.imshow(npimg, interpolation=None)
     plt.show()
+
+def plot_imagenet_patch(patches, targets):
+    upp_l_x = 224 // 2 - 50 // 2
+    upp_l_y = 224 // 2 - 50 // 2
+    bott_r_x = 224 // 2 + 50 // 2
+    bott_r_y = 224 // 2 + 50 // 2
+
+    target_to_classname = {"968": 'cup',
+                           "804": 'soap dispenser',
+                           "806": 'sock',
+                           "923": 'plate',
+                           "954": 'banana',
+                           "585": 'hair spray',
+                           "546": 'electric guitar',
+                           "513": 'brass',
+                           "878": 'typewriter keyboard',
+                           "487": 'cellphone'
+                           }
+
+    fig = plt.figure(figsize=(10, 5))
+
+    for i, (patch, target) in enumerate(list(zip(patches, targets))):
+        patch = np.transpose(patch, (1, 2, 0))
+        patch = patch[upp_l_x:bott_r_x, upp_l_y:bott_r_y, :]
+
+        plt.subplot(2, 5, i+1)
+        plt.title(target_to_classname[target])
+        plt.imshow(patch)
+        plt.axis('off')
+    plt.show()
